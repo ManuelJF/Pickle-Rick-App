539fe7757587bdf3df7a42848297d47544487c0f
Luis Juarez <skaflon_war@hotmail.com>
2020-08-27 19:48:39 -0500

    initial commit and initial rn project

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text>Pickle Rickkkkkkkkkkkkkk!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
